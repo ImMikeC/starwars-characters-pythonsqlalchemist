f2a6e8c9be3b9d5d9f89b47f5f1d20aff27d58bc
ImMikeC <hebressus@gmail.com>
2022-10-10 12:37:32 -0300

    .

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-# 20th commit to github
+# 21th commit to github
 # Urgency and rushing again!!
 
 import os
@@ -9,36 +9,55 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
-Model = declarative_base()
+Base = declarative_base()
 
-class Ships(Model):
+class Person(Base):
+    __tablename__ = 'person'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+
+class Address(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(250))
+    street_number = Column(String(250))
+    post_code = Column(String(250), nullable=True)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+    def to_dict(self):
+        return {}
+
+class Ships(Base):
     __tablename__ = 'ships'
     id = Column(Integer, primary_key=True)
     ships_name = Column(String(250))
     ships_number = Column(String(250))    
+    person = relationship(Person)
 
     def to_dict(self):
         return {}   
 
-class Planets(Model):
+class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    planets_name = Column(String(251))
-    planets_number = Column(String(251))    
+    planets_name = Column(String(250))
+    planets_number = Column(String(250))    
+    person = relationship(Person)
 
     def to_dict(self):
         return {}     
 
-class Films(Model):
+class Films(Base):
     __tablename__ = 'films'
     id = Column(Integer, primary_key=True)
-    films_name = Column(String(251))
+    films_name = Column(String(25))
     films_number = Column(String(251))    
-    director = Column(String(251))
-    productor = Column(String(251))
+    director = Column(String(250))
+    productor = Column(String(250))
 
     def to_dict(self):
         return {} 
 
 ## Draw from SQLAlchemy base
-render_er(Model, 'diagram_n5.png')
+render_er(Base, 'diagram_n5.png')
