676a1048f1ef1bdb6c0fc9161669eda4a747bc03
ImMikeC <hebressus@gmail.com>
2022-10-29 21:43:11 -0300

    .

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
-# Sixth commit to github
-# Urgency and rushing!!
+# Seventh commit to github
+# Urgency and rushing again!!
 
 import os
 import sys
@@ -13,15 +13,11 @@ Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
@@ -34,15 +30,23 @@ class Address(Base):
 
 class Ships(Base):
     __tablename__ = 'ships'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     ships_name = Column(String(250))
     ships_number = Column(String(250))    
     person = relationship(Person)
 
     def to_dict(self):
-        return {}        
+        return {}   
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    planets_name = Column(String(250))
+    planets_number = Column(String(250))    
+    person = relationship(Person)
+
+    def to_dict(self):
+        return {}     
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram_n5.png')
