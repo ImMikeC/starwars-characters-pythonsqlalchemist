c5449e722ecb1ed6695934d49af50be0b306898d
ImMikeC <hebressus@gmail.com>
2022-10-31 00:36:55 -0300

    .

--- src/app.py
+++ src/app.py
@@ -9,14 +9,14 @@ from eralchemy import render_er
 Base = declarative_base()
 
 class Person(Base):
-    __tablename__ = 'perso'
+    __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
 class Address(Base):
-    __tablename__ = 'addres'
+    __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
@@ -30,4 +30,4 @@ class Address(Base):
         return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram_n1.png')
+render_er(Base, 'diagram_n2.png')
